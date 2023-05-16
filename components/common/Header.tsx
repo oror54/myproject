9b0040e2ff79f3204a19bb5bffe93f033bcefd52
jihyeon <jihyeon33@gmail.com>
2023-05-16 17:49:44 +0900

    포트폴리오

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import styles from '@/styles/Header.module.scss';
+import React from "react";
+import styles from "./Header.module.scss";
+import cx from "classnames";
 
 /* Constants =========================================================== */
 /* Prop =========================================================== */
@@ -7,9 +8,19 @@ type Prop = {};
 /* <Header/> =========================================================== */
 export default function Header() {
   return (
-    <div className={styles.header}>
-      <header>Header</header>
-    </div>
+    <header className={cx([styles.header, "header"])}>
+      <div className={styles.header_wrap}>
+        <h1>jihyeon</h1>
+        <ul>
+          <li className="act">
+            <a href="./index.html">home</a>
+          </li>
+          <li>
+            <a href="./sub.html">project</a>
+          </li>
+        </ul>
+      </div>
+    </header>
   );
 }
 Header.defaultProps = {};
